153466a79d39d602501b137db8cb792dab5d2ec8
Abinas Nayak <110822244+abinas94@users.noreply.github.com>
2022-09-08 09:36:49 +0000

    some changes

--- app.py
+++ app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'ID:1,
+  'Title':'Data Analyst',
+  'Location':'Bengaluru'
+  'Salary':'Rs.10,00,000'
+},
+  {
+  'ID:2,
+  'Title':'Data Scientist',
+  'Location':'Pune'
+  'Salary':'Rs.15,00,000'
+},
+  {
+  'ID:3,
+  'Title':'ML Engineer',
+  'Location':'Remote'
+  'Salary':'Rs.18,00,000'
+},
+  {
+  'ID:4,
+  'Title':'Front-End Engineer',
+  'Location':'Remote'
+  'Salary':'Rs.25,00,000'
+}
+]
+
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',openings = JOBS)
 
 
 
